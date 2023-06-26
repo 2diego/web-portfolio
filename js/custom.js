4c059a9307bbf42b1471ad8832959b2cec95d030
2diego <diee.maldonado@gmail.com>
2023-06-26 07:41:42 -0300

    Creating repo

---
+++ js/custom.js
@@ -0,0 +1,40 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#sideNav'
+  });
+
+// Typed Initiate
+if ($('.typed-text-output').length == 1) {
+  var typed_strings = $('.typed-text').text();
+  var typed = new Typed('.typed-text-output', {
+      strings: typed_strings.split('+'),
+      typeSpeed: 30,
+      backSpeed: 10,
+      smartBackspace: false,
+      loop: true
+  });
+}
+
+})(jQuery); // End of use strict
